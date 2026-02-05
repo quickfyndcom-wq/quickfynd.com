b7e4d5949491815ff57673c2aa8557fcf1201e85
quickfyndcom-wq <quickfynd.com@gmail.com>
2026-02-05 18:54:51 +0400

    Update page.jsx

--- app/(public)/page.jsx
+++ app/(public)/page.jsx
@@ -52,7 +52,7 @@ export default function Home() {
 
     return (
         <>
-                <HomeCategories/>
+                {/* <HomeCategories/> */}
                 <Hero />
                 <LatestProducts />
                                      <CarouselSlider/>
